0544a6075359d6be70f1018b66d9df30a5a20e37
Erich Querner <erichquerner@posteo.org>
2025-08-29 11:45:28 +0200

    start working on tuio 20 processor

---
+++ src/tuio20/processor.rs
@@ -0,0 +1,51 @@
+use std::{
+    cell::Cell,
+    net::Ipv4Addr,
+    sync::mpsc::{self, Receiver},
+};
+
+use rosc::OscPacket;
+
+use crate::common::{client::Client, osc_receiver::OscReceiver, tuio_time::TuioTime};
+
+pub struct Processor<R>
+where
+    R: OscReceiver<OscPacket> + Send + Sync + 'static,
+{
+    client: Client<R>,
+    packet_receiver: Receiver<OscPacket>,
+    current_frame: Cell<i32>,
+    current_time: Cell<TuioTime>,
+}
+
+impl<R> Processor<R>
+where
+    R: OscReceiver<OscPacket> + Send + Sync + 'static,
+{
+    pub fn new(remote: Ipv4Addr, port: u16) -> anyhow::Result<Self> {
+        let (sender, receiver) = mpsc::channel();
+        let client = Client::<R>::new(remote, port, sender)?;
+        Ok(Self {
+            current_frame: (-1).into(),
+            current_time: Cell::new(TuioTime::from_system_time()?),
+            client,
+            packet_receiver: receiver,
+        })
+    }
+
+    pub fn connect(&self) -> anyhow::Result<()> {
+        self.client.connect()?;
+        Ok(())
+    }
+
+    pub fn update(&self) -> anyhow::Result<()> {
+        let packet = self.packet_receiver.recv()?;
+        self.process_packet(packet)?;
+        Ok(())
+    }
+
+    fn process_packet(&self, packet: OscPacket) -> anyhow::Result<()> {
+        println!("{packet:?}");
+        Ok(())
+    }
+}
