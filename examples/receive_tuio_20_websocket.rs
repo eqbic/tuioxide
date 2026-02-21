1e3b6eb5337ae8c9d3ba753b3b9e5270241c06a9
qbic <erichquerner@posteo.org>
2026-02-21 19:21:47 +0100

    create tuio 2 example with websocket

---
+++ examples/receive_tuio_20_websocket.rs
@@ -0,0 +1,51 @@
+use std::io;
+
+use tuioxide::{
+    client::{WebsocketOscReceiver, tuio20::client::Client},
+    core::tuio20::events::{PointerEvent, TokenEvent},
+};
+
+fn main() -> Result<(), io::Error> {
+    let mut client = Client::new(WebsocketOscReceiver::default());
+
+    loop {
+        let events = client.update()?;
+        for event in events.pointer_events {
+            match event {
+                PointerEvent::Add(pointer) => println!(
+                    "New pointer [{}] at position {:?}",
+                    pointer.session_id(),
+                    pointer.position()
+                ),
+                PointerEvent::Update(pointer) => println!(
+                    "Update pointer[{}] -> {:?}",
+                    pointer.session_id(),
+                    pointer.position()
+                ),
+                PointerEvent::Remove(pointer) => {
+                    println!("Remove pointer[{}]", pointer.session_id())
+                }
+            }
+        }
+
+        for event in events.token_events {
+            match event {
+                TokenEvent::Add(token) => println!(
+                    "New token [{}] with id {} at position {:?}",
+                    token.session_id(),
+                    token.component_id(),
+                    token.position()
+                ),
+                TokenEvent::Update(token) => println!(
+                    "Update token[{}] with id {} -> {:?}",
+                    token.session_id(),
+                    token.component_id(),
+                    token.position()
+                ),
+                TokenEvent::Remove(token) => {
+                    println!("Remove token[{}]", token.session_id())
+                }
+            }
+        }
+    }
+}
