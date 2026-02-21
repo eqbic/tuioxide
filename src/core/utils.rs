87763e0c2a5adef88fa4673c0968cc3daf9a5c57
qbic <erichquerner@posteo.org>
2026-02-21 17:35:59 +0100

    change to work without copy constraint

--- src/core/utils.rs
+++ src/core/utils.rs
@@ -1,14 +1,22 @@
 use std::collections::{HashMap, HashSet};
 
-pub fn retain_alive<T>(current: &mut HashMap<i32, T>, alive: &HashSet<i32>) -> Vec<T>
-where
-    T: Copy,
-{
-    let to_remove: Vec<T> = current
-        .iter()
-        .filter(|(key, _)| !alive.contains(key))
-        .map(|(_, v)| *v)
+pub fn retain_alive<T>(current: &mut HashMap<i32, T>, alive: &HashSet<i32>) -> Vec<T> {
+    let keys_to_remove: Vec<i32> = current
+        .keys()
+        .filter(|key| !alive.contains(key))
+        .copied()
         .collect();
-    current.retain(|key, _| alive.contains(key));
-    to_remove
+
+    keys_to_remove
+        .into_iter()
+        .filter_map(|key| current.remove(&key))
+        .collect()
+
+    // let to_remove: Vec<T> = current
+    //     .iter()
+    //     .filter(|(key, _)| !alive.contains(key))
+    //     .map(|(_, v)| *v)
+    //     .collect();
+    // current.retain(|key, _| alive.contains(key));
+    // to_remove
 }
